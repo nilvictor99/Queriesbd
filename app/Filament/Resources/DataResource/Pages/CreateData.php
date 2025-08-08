de27ca3525d8b7259dbbc14969ed773737234955
victor <nilvictor99@gmail.com>
2025-08-08 12:36:32 -0500

    refactor: Remove unused imports from DataResource and CreateData pages

--- app/Filament/Resources/DataResource/Pages/CreateData.php
+++ app/Filament/Resources/DataResource/Pages/CreateData.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\DataResource\Pages;
 
 use App\Filament\Resources\DataResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateData extends CreateRecord
